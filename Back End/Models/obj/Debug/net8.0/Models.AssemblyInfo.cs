b9a88754c0db8c2337aa0025e46ed584a08978a9
last0x01 <290016262+last0x01@users.noreply.github.com>
2026-01-08 12:51:24 +0300

    small change

--- Back End/Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Back End/Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b143ba13a33d19af4796e5bd5e19e8198c2e12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94b6822bd340a7aca8035638a5565c9f2eb157a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
